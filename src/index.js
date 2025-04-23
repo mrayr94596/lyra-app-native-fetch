979195043adeb3dc8de72f74ac34c386dc748ecb
Mike Reynolds <mike.reynolds@kofax.com>
2025-04-22 18:50:30 -0700

    index.js improvements

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./App.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Root element not found");
+}
+
+const root = ReactDOM.createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
